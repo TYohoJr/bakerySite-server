2e384c453c78ef7d109c5e21109c8ff0646c00cf
Thomas Yoho <TYohoJr@Gmail.com>
2018-03-30 14:01:43 -0600

    first commit

---
+++ express.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+// var bcrypt = require('bcrypt');
+// const saltRounds = 10;
+// var jwt = require('jsonwebtoken');
+var path = require('path')
+require('dotenv').config();
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// function verifyToken(req, res, next) {
+//     var token = req.body.token;
+//     if (token) {
+//         jwt.verify(token, "Secret", (err, decode) => {
+//             if (err) {
+//                 res.send("Wrong token")
+//             } else {
+//                 res.locals.decode = decode
+//                 next();
+//             }
+//         })
+//     } else {
+//         console.log("no token")
+//         res.send("No token")
+//     }
+// }
+
+// Only has the server listening if it can successfully connect to the database
+MongoClient.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds261838.mlab.com:61838/dine-amite`, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db("dine-amite") // whatever your database name is
+    app.listen(process.env.PORT || 8080, () => {
+        let activePORT = process.env.PORT;
+        if (activePORT) {
+            console.log(`listening on ${activePORT}`)
+        } else {
+            console.log(`listening on http://localhost:8080/`)
+        }
+    })
+})
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html")
+})
